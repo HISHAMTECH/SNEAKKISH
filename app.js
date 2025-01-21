3b70d3c500e913dfbb2efaa6aaf179b98fd20ab3
Hisham <hishamgafu@gmail.com>
2025-01-21 12:21:01 +0530

    view engine setup

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express=require('express')
 const env=require('dotenv').config()
+const path=require('path')
 const db=require('./confiq/db')
+const ejs=require('ejs')
+const userRoute=require("./routes/userRoutes")
 const app=express()
 
 db()
 
+//bodyparser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use('/', userRoute);
+
+//viewengine
+app.set("view engine","ejs")
+app.set("views",[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')])
+app.use(express.static(path.join(__dirname,'public')))
+
+
+
+
 app.listen(process.env.PORT,()=>{
-    console.log("server running");
+ console.log("server running");
     
 })
 
